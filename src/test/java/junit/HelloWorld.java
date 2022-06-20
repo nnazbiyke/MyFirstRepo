3e34394cf01f9487204e9ea3a07ae55fe7c82cb3
Nazbiyke <nnazbiyke@bk.ru>
2022-06-20 19:21:09 +0600

    Added Hello World

---
+++ src/test/java/junit/HelloWorld.java
@@ -0,0 +1,7 @@
+package junit;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/junit/TestGit.java
@@ -0,0 +1,10 @@
+package junit;
+
+public class TestGit {
+    public static void main(String[] args){
+
+        System.out.println("Hello World!");
+    }
+
+
+}
